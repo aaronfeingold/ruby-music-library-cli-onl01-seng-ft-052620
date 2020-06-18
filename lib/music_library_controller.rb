3ea6e357faaa75e730337d7b2b98a5688306a302
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-18 22:16:48 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -8,34 +8,38 @@ class MusicLibraryController
     main_menu
   end
   
-  def main_menu 
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artists'."
-    input = gets.strip
-    
-    case input
-    when "list songs"
-      list_songs 
-      main_menu
-    when "list artist"
-      list_artists
-      main_menu
-    when "list genre"
-      list_genres
-      main_menu
-    when "play song"
-      play_song
-      main_menu
-    when "exit"
-      exit
-    else
-      puts"invalid choice"
-      main_menu
-    end 
-  end 
+    def call
+    input = ""
+
+    while input != "exit"
+      puts "Welcome to your music library!"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
+
+      input = gets.strip
+
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_songs_by_artist
+      when "list genre"
+        list_songs_by_genre
+      when "play song"
+        play_song
+      end
+    end
+  end
   
   def list_songs
     Song.alphabetized.each.with_index(1) do |song, index|
